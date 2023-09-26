8709ab62eb790230f792cc2232f98643dd1609f8
Adnan <adnanjamilrajpoot@gmail.com>
2023-09-26 06:33:25 +0500

    add some examples

--- functions.js
+++ functions.js
@@ -3,4 +3,13 @@
 function multiply(a, b=0){
     return (2* (a + b));
 }
-multiply(3)
+multiply(3);
+
+// Rest Parameters
+// function diffination can have only one rest parameter for example (...y)
+// Rest parameters must be the last parameter
+function collectThings(a, ...y){ 
+    console.log(a);
+    console.log(y);
+}
+collectThings(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
