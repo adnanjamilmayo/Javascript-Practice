6db6b5f76248d36296e533697eab6356b147c17c
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-17 18:00:46 +0500

    conversions of data types

---
+++ ConversionOprations.js
@@ -0,0 +1,21 @@
+let score = "44abc";
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+ let valueInNumber = Number(score)
+  
+ console.log(valueInNumber)
+
+//  33 => 33
+// "444abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1; => ture
+//  let isLoggedIn = "" => false
+let isLoggedIn = "adnan";
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanLoggedIn)
