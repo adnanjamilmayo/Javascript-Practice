42768229e937c86ba5eaa8242dcdc9efe979e2a9
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-18 21:29:44 +0500

    numbers and maths

---
+++ Math.js
@@ -0,0 +1,19 @@
+//Round
+// ciel
+// floor
+// abc
+// Min
+// Random => most important and commenly use
+
+
+const min = 9;
+const max = 100;
+
+console.log(Math.floor((Math.random()*10 + 1)));
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  console.log(getRandomInt(3));
+
+  getRandomInt()
