a987816777388bcfac7ff6419dcc7951d866d4c1
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-21 00:54:33 +0500

    types

--- Objects.js
+++ Objects.js
@@ -1,38 +1,51 @@
-// Object
+// // Object
 
-const product = {
-    itemName: "phone",
-    price: 35000,
-    discount: 5000,
-    itemCode: "F40"
-}
+// const product = {
+//     itemName: "phone",
+//     price: 35000,
+//     discount: 5000,
+//     itemCode: "F40"
+// }
 
-// factory fucntion
+// // factory fucntion
 
-function createProduct(name, price, discount, itemCode){
-    return {
-        itemCode: name,
-        price: price,
-        discount: discount,
-        itemCode: itemCode
-    }
-}
+// function createProduct(name, price, discount, itemCode){
+//     return {
+//         itemCode: name,
+//         price: price,
+//         discount: discount,
+//         itemCode: itemCode
+//     }
+// }
 
-const footBall = createProduct("footBall", 4000, 1000, "f20")
-console.log(footBall);
+// const footBall = createProduct("footBall", 4000, 1000, "f20")
+// console.log(footBall);
 
-// constructer function
+// // constructer function
 
-function Product(name, price, discount, itemCode){
-    this.itemCode = name;
-    this.price = price;
-    this.discount = discount;
-    this.itemCode = itemCode;
-    this.discountValue = function(){
-        return price * discount/100;
-    }
-}
+// function Product(name, price, discount, itemCode){
+//     this.itemCode = name;
+//     this.price = price;
+//     this.discount = discount;
+//     this.itemCode = itemCode;
+//     this.discountValue = function(){
+//         return price * discount/100;
+//     }
+// }
 
-const samsungPhone = Product("samsungA30", 3400, 900, "y7")
+// const samsungPhone = Product("samsungA30", 3400, 900, "y7")
 
-console.log(samsungPhone);
+// console.log(samsungPhone);
+
+const users = [
+    { name: "Alice", age: 30, email: "alice@example.com" },
+    { name: "Bob", age: 25, email: "bob@example.com" },
+    { name: "Charlie", age: 35, email: "charlie@example.com" },
+  ];
+  
+  function extractEmails(users) {
+    // Your code here
+  }
+  
+  const emails = extractEmails(users);
+  console.log(emails);
--- functions.js
+++ functions.js
@@ -1,15 +1,6 @@
-// Default Parameters Values
-
-function multiply(a, b=0){
-    return (2* (a + b));
+function calculateCartPrice(...num1){
+    return num1
 }
-multiply(3);
+console.log(calculateCartPrice(12));
 
-// Rest Parameters
-// function diffination can have only one rest parameter for example (...y)
-// Rest parameters must be the last parameter
-function collectThings(a, ...y){ 
-    console.log(a);
-    console.log(y);
-}
-collectThings(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+ffor
