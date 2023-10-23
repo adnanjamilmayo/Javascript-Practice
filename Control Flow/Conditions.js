a0b3808ac6765ecd28b649b47b73415a0b23e95b
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-24 00:56:48 +0500

    types

---
+++ Control Flow/Conditions.js
@@ -0,0 +1,13 @@
+// Switch Statement
+
+const fullName = "Muhammad Adnan"
+
+switch (fullName) {
+    case "Muhammad Ali":
+        console.log("yes im adnan")
+        break;
+
+    default:
+        console.log("muhammad adnan");
+        break;
+}
---
+++ Control Flow/filter.js
@@ -0,0 +1,6 @@
+const departments = ["AI","library","Business","socialogy","english"]
+
+const deprtlist= departments.filter( (deprtnamess) => {
+    return deprtnamess => 2
+})
+console.log(deprtlist);
