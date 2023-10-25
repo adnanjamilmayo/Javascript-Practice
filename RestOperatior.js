3d15318b19d14c55a0d22cb4fc66302ef26576ca
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-25 19:02:30 +0500

    rest operators question solved

---
+++ RestOperatior.js
@@ -0,0 +1,27 @@
+// // function sum(name, adress, ...args){
+// //     console.log(arguments);
+
+// //     console.log(`My Name is: ${name} and my adress is: ${adress}`);
+// //     let sum =0;
+// //     for (let  i in args) {
+// //         sum += args[i]
+// //     }
+// //     console.log(sum);
+// // }
+
+// // sum("Muhammad Adnan","Pakistan",12,34,32)
+
+
+// function findLargest(...numbers) {
+//     let largest = -Infinity;
+    
+//     for (let number of numbers) {
+//       if (number > largest) {
+//         largest = number;
+//       }
+//     }
+    
+//     console.log(largest);
+//   }
+  
+//   findLargest(12,3,2,32,32,3,2,3232323)
