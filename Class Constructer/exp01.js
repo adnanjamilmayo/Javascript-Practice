e264df5f368b4dbd5ef2c7112b6f93b9a77e07b5
Adnan <adnanjamilrajpoot@gmail.com>
2023-11-22 02:12:17 +0500

    intial commit

--- Class Constructer/exp01.js
+++ Class Constructer/exp01.js
@@ -8,8 +8,12 @@ class user{
     encryptPassword(){
         return `${this.password}abc`
     }
+    changeUserNAme(){
+        return `${this.username.toUpperCase()}`
+    }
 }
 
 const fullName = new user("Muhammad Adnan", "adnan@gmail.com", "123")
 
 console.log(fullName.encryptPassword());
+console.log(fullName.changeUserNAme());
